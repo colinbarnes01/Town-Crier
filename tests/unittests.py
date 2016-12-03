7b2156584d531cb2945e89f6c7ee23b256827454
Colin <tuf84887@temple.edu>
2016-12-03 13:04:50 -0500

    renamed some directories to be more pythonic and also added scraper.getHeadlines function

--- tests/unittests.py
+++ tests/unittests.py
@@ -6,6 +6,11 @@ TEST CASES
 
 
 """
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+os.sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 import unittest
 import requests
@@ -17,7 +22,7 @@ from requester import Requester
 class TestScraping(unittest.TestCase):
 	requester = Requester()
 	scraper = Scraper()
-
+	"""
 	def test_getHtmlBinary(self):
 		binary = self.requester.getHtmlBinary('http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html')
 		knownBinary = requests.get('http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html')
@@ -28,6 +33,11 @@ class TestScraping(unittest.TestCase):
 		r = requests.get('http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html')
 		urls = self.scraper.scrapeUrls(r.content)
 		self.assertEqual(urls[0], 'http://www.yahoo.com/')
+	"""
+	def test_getHeadlines(self):
+		r = requests.get('https://news.google.com/')
+		headlines = self.scraper.scrapeHeadlines(r.content)
+
 	
 
 
