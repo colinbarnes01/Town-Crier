54c0d8fdc4188d24d5885c738e29f79855e1275c
Colin <tuf84887@temple.edu>
2016-12-10 10:46:53 -0500

    addin keys to heroku

---
+++ keys.py
@@ -0,0 +1,13 @@
+""" This file holds a python dictionary containing
+the authentication keys for the twitter api.  The
+file is kept seprarate to provide modularity for
+the AccountManager class and also because this file
+will not be uploaded to my public github.
+"""
+
+keys = {
+"CONSUMER_KEY":"Zh8o9RVKTIlCBrOcuTxjQTz0m",
+"CONSUMER_KEY_SECRET":"Db8FAjh9LZi7rIOm5qXR9rf8d4FYB0aCnHLFyBAZ76O3tZk0r0",
+"ACCESS_TOKEN":"790686650267496448-xzlIIBZjSFHmk1qXD6oVA6wOZeRyEZB",
+"ACCESS_TOKEN_SECRET":"L9Tl6ioxAkam6r3Cka2srXDn9wBhFGcUs5BQgeS45fzql"
+}
